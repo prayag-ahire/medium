ae659bce36f8589fbe061d9e74d01a9882c5bc53
prayag <prayagahire365@gmail.com>
2025-04-15 22:37:44 +0530

    solve 9

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -1,5 +1,5 @@
 
-import { BlogPage } from "../components/blogpage.tsx";
+import { BlogPage } from "../components/BlogPage";
 import { useBlog, } from "../hooks"
 import { useParams } from "react-router-dom";
 
